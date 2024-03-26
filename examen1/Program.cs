7f30ff5796d005deaf7b92621a7c59bacd3369d3
cirilo 0 <rodriguezciriloch2000@gmail.com>
2024-03-26 17:37:45 -0400

    examen

---
+++ examen1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero = 12345;
+        int resultado = MoverNumeroDelMedioAlPrincipio(numero);
+        Console.WriteLine(resultado);
+    }
+
+    static int MoverNumeroDelMedioAlPrincipio(int numero)
+    {
+        string numeroString = numero.ToString();
+        int longitud = numeroString.Length;
+        int mitad = longitud / 2;
+        
+        char numeroDelMedio = (longitud % 2 == 0) ? numeroString[mitad - 1] : numeroString[mitad];
+        
+        string restoNumero = numeroString.Remove(mitad, 1);
+        string numeroMovidoString = numeroDelMedio + restoNumero;
+        
+        return int.Parse(numeroMovidoString);
+    }
+}
